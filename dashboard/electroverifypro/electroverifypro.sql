cbc657c3812fb769193337de8eaeaed2f8d3e2fd
Taha Mahmood <tahamahmood004@gmail.com>
2024-01-31 04:18:04 +0500

    sql file updated

--- dashboard/electroverifypro/electroverifypro.sql
+++ dashboard/electroverifypro/electroverifypro.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jan 11, 2024 at 08:22 PM
+-- Generation Time: Jan 31, 2024 at 12:16 AM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.2.4
 
@@ -24,6 +24,75 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `admins`
+--
+
+CREATE TABLE `admins` (
+  `id` int(11) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `reg_date` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `admins`
+--
+
+INSERT INTO `admins` (`id`, `username`, `password`, `reg_date`) VALUES
+(3, 'dadadad', '$2y$10$yZdczzCivPMV2jFghqWz.uKTFBoCR.WqJYP2.awVnxJQEbcvMDaWG', '2024-01-31 03:38:39');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `products`
+--
+
+CREATE TABLE `products` (
+  `id` int(11) NOT NULL,
+  `p_name` varchar(255) NOT NULL,
+  `price` int(11) NOT NULL,
+  `product_id` bigint(20) NOT NULL,
+  `image` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `products`
+--
+
+INSERT INTO `products` (`id`, `p_name`, `price`, `product_id`, `image`) VALUES
+(5, 'taha', 123, 2147483647, ''),
+(7, 'resister', 4554, 6454795194, ''),
+(9, '1', 1, 5413798100, ''),
+(10, '1', 1, 8270283763, ''),
+(11, '1', 1, 1405244424, '');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `testing`
+--
+
+CREATE TABLE `testing` (
+  `id` int(11) NOT NULL,
+  `p_name` varchar(255) NOT NULL,
+  `product_id` int(20) NOT NULL,
+  `testing_id` bigint(20) NOT NULL,
+  `image` varchar(255) NOT NULL,
+  `result` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `testing`
+--
+
+INSERT INTO `testing` (`id`, `p_name`, `product_id`, `testing_id`, `image`, `result`) VALUES
+(2, 'taha', 5, 885980042519, '', 1),
+(5, 'resister', 11, 298376413156, '', 1),
+(6, '1', 9, 858213693017, '', 3);
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `users`
 --
 
@@ -39,15 +108,35 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `username`, `password`, `reg_date`) VALUES
-(6, 'taha', 'taha1234', '2024-01-10 20:52:59'),
-(7, 'muneeb', 'muneeb1234', '2024-01-10 20:53:48'),
-(14, 'mahnoor', '$2y$10$UIDaTfdjxYFi/u4neFFcUu21Y/R3772DZWQM/a2CKNiS7SAqwJbGO', '2024-01-10 21:04:01'),
-(20, 'taha1', '$2y$10$6tjyeotEDDbjRXqQceImaek.C70ITfKD9JXTeVe2vmCm7HgCyRccy', '2024-01-10 21:15:55'),
-(22, 'areeb', '$2y$10$dian82jZY4lsvP97OFS/Reoh0ZULDKj0.hZoubCp.YeCfxXdvJXdC', '2024-01-11 17:16:20');
+(37, 'asdasdasjdahsud', '$2y$10$yv6KGVENh9bnOlIBRTpVS.EVghBqjcrdqcGsgXb1FNZjVX4dP.QeW', '2024-01-19 02:57:35'),
+(38, 'mahnoor', '$2y$10$ZYjZHuBoLeJqjkHNWuU1DeL.j2zXRYi9kyl1M6dat.PXSNscQaLsC', '2024-01-19 02:57:46'),
+(39, 'tahaaa', '$2y$10$VB49B0RwGSGML0PZHHZjqOYo.SRGPQzKq2H2gYqvKP4lq/WDPYwLO', '2024-01-19 02:59:24');
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `admins`
+--
+ALTER TABLE `admins`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `username` (`username`);
+
+--
+-- Indexes for table `products`
+--
+ALTER TABLE `products`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `product_id` (`product_id`);
+
+--
+-- Indexes for table `testing`
+--
+ALTER TABLE `testing`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `testing_id` (`testing_id`),
+  ADD UNIQUE KEY `product_id` (`product_id`);
 
 --
 -- Indexes for table `users`
@@ -61,10 +150,38 @@ ALTER TABLE `users`
 --
 
 --
+-- AUTO_INCREMENT for table `admins`
+--
+ALTER TABLE `admins`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `products`
+--
+ALTER TABLE `products`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+
+--
+-- AUTO_INCREMENT for table `testing`
+--
+ALTER TABLE `testing`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=41;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `testing`
+--
+ALTER TABLE `testing`
+  ADD CONSTRAINT `fk_testing_product` FOREIGN KEY (`product_id`) REFERENCES `products` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
